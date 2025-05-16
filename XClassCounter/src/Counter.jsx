817bb83776d41eed909a10c72fe61ab082f923c2
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-16 22:14:32 +0530

    initial try

---
+++ XClassCounter/src/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count > 0 ? prevState.count - 1 : 0,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
